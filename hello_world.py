99c6465035528bebfa4ad439458830acc1b46f4a
mohammedengg <mohammedibrahim071997@gmail.com>
2026-01-15 19:45:47 -0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+#Store values in variables
+name = "IBRAHIM MOHAMMED"
+role = "Python intern"
+date = "01/15/2026"
+
+#Print the values
+print("Name:", name)
+print("Role:", role)
+print("Date:", date)
